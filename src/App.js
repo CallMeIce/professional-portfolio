b0912e4cf21148ff2bbe98dbcbe7165cdd7537ea
Isaak Morales <ice.isaak97@gmail.com>
2023-01-12 17:28:40 -0500

    getting routes set and making the home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+// import { Route } from '@mui/icons-material';
+// import { Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <main>
+                <h1>Isaak Morales</h1>
+                <p>Upcoming Full Stack Web Developer, Upcoming Salesforce Admin/Dev, Drummer, Gamer</p>
+                </main>
+
+        </div>
+    )
+}
