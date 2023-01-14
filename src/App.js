f0eacc06a7033c109a04a35467de224e98303119
Isaak Morales <ice.isaak97@gmail.com>
2023-01-14 03:12:58 -0500

    Getting the route to work and image to appear

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-// import { Route } from '@mui/icons-material';
-// import { Routes } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
+import { Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+import "./App.css";
 import React from "react";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+    <div className="routesContainer">
+      <Routes>
+        <Route path="/" component={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
