f1f97b2f745c07e8b6ba159cc53161d6d06c6ca1
Isaak Morales <ice.isaak97@gmail.com>
2023-01-23 07:46:46 -0500

    Added the projects route and edited my about me

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
 import React from "react";
 import About from './components/AboutMe';
+import Projects from './components/Projects';
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" component={<Home />} />
         <Route path="/AboutMe" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
       </Routes>
     </div>
   );
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -21,8 +21,9 @@ export default function About() {
   return (
     <div style={styles.aboutStyle}> 
     <p>A graduate from Lincoln Technical Institute with a diploma in Automotive Technology who later found out</p>
-    <p>that he wanted a career change and decided to pursue Full-Stack Web Development. I still have</p>
-    <p>my passion in cars but would like to keep that as a hobby.</p>
+    <p>that he wanted a career change and decided to pursue Full-Stack Web Development with a programming</p>
+    <p>bootcamp through UPenn. I still have my passion in cars but would like to keep that as a hobby. </p>
+    <p>Favorite car of all time would have to be a 98-02 Pontiac Firebird WS6.</p>
     <p>Some technical skills I have acquired are: HTML, CSS, Javascript, Bootstrap, Jquery, Nodejs, React, Express, and Git.</p>
     <img style={styles.imageStyle} src={Me} alt='Me' />
     </div>
