91c76bf3d88de28bd8c8acfbc5d5889a1ce8c666
Isaak Morales <ice.isaak97@gmail.com>
2023-01-23 06:30:50 -0500

    Added the Aboutme route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
-import "./App.css";
 import React from "react";
+import About from './components/AboutMe';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="routesContainer">
       <Routes>
         <Route path="/" component={<Home />} />
+        <Route path="/AboutMe" element={<About />} />
       </Routes>
     </div>
   );
